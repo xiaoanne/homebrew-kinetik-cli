6b828419bf20fb34bcd6646ddca7c7601e969a12
Anne Wang <huiyang.wang621@gmail.com>
2023-04-06 10:38:55 +1200

    a

--- kinetik-cli.rb
+++ kinetik-cli.rb
@@ -8,7 +8,7 @@ class KinetikCli < Formula
     url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.7/kinetik--0.13.5.arm64_monterey.bottle.tar.gz"
     sha256 "ef901473181d1f5c638bf613b54b09ea69e65a6bbaccd2c25e30de25f669e40f"
   else
-    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.6/kinetik--0.13.5.x86_64_monterey.bottle.tar.gz"
+    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.7/kinetik--0.13.5.arm64_monterey.bottle.tar.gz"
     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
   end
   bottle do
