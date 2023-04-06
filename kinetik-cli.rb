28bf34bd194eec45ed1b5a8091f5b03ffb839b26
Anne Wang <huiyang.wang621@gmail.com>
2023-04-06 16:10:15 +1200

    a

--- kinetik-cli.rb
+++ kinetik-cli.rb
@@ -10,7 +10,7 @@ class KinetikCli < Formula
     url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryDownloadStrategy
     sha256 "ef901473181d1f5c638bf613b54b09ea69e65a6bbaccd2c25e30de25f669e40f"
   else
-    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.6/kinetik--0.13.5.x86_64_monterey.bottle.tar.gz"
+    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin"
     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
   end
   bottle do
@@ -25,9 +25,9 @@ class KinetikCli < Formula
 
   def install
     if Hardware::CPU.arm?
-      bin.install "0.13.5/bin/kinetik" => "kinetik"
+      bin.install "kinetik-main-aarch64-apple-darwi" => "kinetik"
     else
-      bin.install "0.13.5/bin/kinetik-main-x86_64-apple-darwin" => "kinetik"
+      bin.install "kinetik-main-x86_64-apple-darwin" => "kinetik"
     end
     chmod 0755, "#{bin}/kinetik"
     system "xattr", "-dr", "com.apple.quarantine", "#{bin}/kinetik"
