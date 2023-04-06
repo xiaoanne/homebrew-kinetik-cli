e258c28b5108bcf9a9ad7a0cb6f15939c3bdf267
Anne Wang <huiyang.wang621@gmail.com>
2023-04-06 16:37:04 +1200

    a

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -2,28 +2,29 @@ require "formula"
 require_relative "../lib/custom_download_strategy"
 
 class KinetikCli < Formula
-  desc "Kinetik"
-  homepage "https://github.com/xiaoanne/homebrew-d3"
+  desc "Kinetik CLI"
+  homepage "https://github.com/section6nz/homebrew-kinetik-base"
   version "0.13.5"
-  license "section6nz"
-  head "https://github.com/section6nz/kinetik-base.git", branch: "main"
+  license "Apache-2.0"
+  head "https://github.com/section6nz/homebrew-kinetik-base", branch: "main"
+
   if Hardware::CPU.arm?
-    # url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.7/kinetik--0.13.5.arm64_monterey.bottle.tar.gz"
     url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
   else
     url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
   end
+
   bottle do
     root_url "https://ghcr.io/v2/section6nz/kinetik-base"
   end
 
   def install
     if Hardware::CPU.arm?
-      bin.install "kinetik-main-aarch64-apple-darwin" => "kinetik"
+      bin.install "#{version}/bin/kinetik-main-aarch64-apple-darwin" => "kinetik"
     else
-      bin.install "kinetik-main-x86_64-apple-darwin" => "kinetik"
+      bin.install "#{version}/bin/kinetik-main-x86_64-apple-darwin" => "kinetik"
     end
     chmod 0755, "#{bin}/kinetik"
     system "xattr", "-dr", "com.apple.quarantine", "#{bin}/kinetik"
