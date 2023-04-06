b79fa23a0af9b9d13c92425b1548b8f4fc12716c
Anne Wang <huiyang.wang621@gmail.com>
2023-04-06 16:09:24 +1200

    a

--- kinetik-cli.rb
+++ kinetik-cli.rb
@@ -4,7 +4,7 @@ class KinetikCli < Formula
   homepage "https://github.com/xiaoanne/homebrew-d3"
   version "0.13.5"
   license "section6nz"
-  head "https://github.com/xiaoanne/homebrew-d3", branch: "main"
+  head "https://github.com/section6nz/kinetik-base", branch: "main"
   if Hardware::CPU.arm?
     # url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.7/kinetik--0.13.5.arm64_monterey.bottle.tar.gz"
     url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryDownloadStrategy
@@ -14,7 +14,7 @@ class KinetikCli < Formula
     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
   end
   bottle do
-    root_url "https://ghcr.io/v2/xiaoanne/homebrew-d3"
+    root_url "https://ghcr.io/v2/section6nz/kinetik-base"
   end
 
   strategy = GitHubPrivateRepositoryDownloadStrategy.new(
