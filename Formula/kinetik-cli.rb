f4cf14185ec35f60c2e2d2f6a77da513513ca69d
Anne Wang <huiyang.wang621@gmail.com>
2023-04-11 11:30:55 +1200

    Try PAT

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -8,16 +8,16 @@ class KinetikCli < Formula
   head "https://github.com/section6nz/homebrew-kinetik-base", branch: "main"
 
   if Hardware::CPU.arm?
-    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", using: GithubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
   else
-    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", using: GithubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
   end
-
-  bottle do
-    root_url "https://ghcr.io/v2/section6nz/kinetik-base"
-  end
+#
+#   bottle do
+#     root_url "https://ghcr.io/v2/section6nz/kinetik-base"
+#   end
 
   def install
     if Hardware::CPU.arm?
