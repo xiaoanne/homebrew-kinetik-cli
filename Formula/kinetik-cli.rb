9a2e9c9a878630f17362fa6901a2de7d4612f94c
Anne Wang <huiyang.wang621@gmail.com>
2023-04-11 11:51:27 +1200

    Try PAT

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -7,27 +7,40 @@ class KinetikCli < Formula
   license "Apache-2.0"
   head "https://github.com/section6nz/homebrew-kinetik-base", branch: "main"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
-  else
-    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
-  end
-#
-#   bottle do
-#     root_url "https://ghcr.io/v2/section6nz/kinetik-base"
-#   end
+  on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
+      else
+        url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
+      end
+    #
+    #   bottle do
+    #     root_url "https://ghcr.io/v2/section6nz/kinetik-base"
+    #   end
 
-  def install
-    if Hardware::CPU.arm?
-      bin.install "kinetik-main-aarch64-apple-darwin" => "kinetik"
-    else
-      bin.install "kinetik-main-x86_64-apple-darwin" => "kinetik"
-    end
-    chmod 0755, "#{bin}/kinetik"
-    system "xattr", "-dr", "com.apple.quarantine", "#{bin}/kinetik"
+      def install
+        if Hardware::CPU.arm?
+          bin.install "kinetik-main-aarch64-apple-darwin" => "kinetik"
+        else
+          bin.install "kinetik-main-x86_64-apple-darwin" => "kinetik"
+        end
+        chmod 0755, "#{bin}/kinetik"
+        system "xattr", "-dr", "com.apple.quarantine", "#{bin}/kinetik"
+      end
   end
+
+  on_linux do
+    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-unknown-linux-gnu", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
+    def install
+        bin.install "kinetik-main-x86_64-unknown-linux-gnu" => "kinetik"
+        chmod 0755, "#{bin}/kinetik"
+        system "xattr", "-dr", "com.apple.quarantine", "#{bin}/kinetik"
+    end
+  end
+
 
   test do
     system "#{bin}/kinetik", "--version"
