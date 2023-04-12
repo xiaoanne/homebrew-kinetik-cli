28da93ca8d7f1eb966977358028222ab9c7ccb7b
Anne Wang <huiyang.wang621@gmail.com>
2023-04-12 12:24:33 +1200

    Update sha value

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -9,10 +9,10 @@ class KinetikCli < Formula
 
   on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
         sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
       else
-        url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
         sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
       end
 
@@ -28,7 +28,7 @@ class KinetikCli < Formula
   end
 
   on_linux do
-    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-unknown-linux-gnu", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-x86_64-unknown-linux-gnu", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "4f49bb353d920a01fd4fc3e2689c70f900848c35afb85bc72e2fb683bcb09ffd"
     def install
         bin.install "kinetik-main-x86_64-unknown-linux-gnu" => "kinetik"
