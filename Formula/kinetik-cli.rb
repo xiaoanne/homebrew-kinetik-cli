da474e3c347f52aafd9ff868c4c6c797cd122857
Anne Wang <huiyang.wang621@gmail.com>
2023-04-13 11:11:10 +1200

    Update sha value

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -9,10 +9,10 @@ class KinetikCli < Formula
 
   on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-aarch64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-aarch64-apple-darwin"
         sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
       else
-        url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-x86_64-apple-darwin", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        url "https://github.com/section6nz/kinetik-base/releases/download/v#{version}/kinetik-main-x86_64-apple-darwin"
         sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
       end
 
