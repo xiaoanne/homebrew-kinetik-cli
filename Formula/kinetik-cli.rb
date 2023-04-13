31e7a116199fab2a410be1c91d70ba45253c67a6
Anne Wang <huiyang.wang621@gmail.com>
2023-04-14 09:46:55 +1200

    a

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -21,7 +21,7 @@ class KinetikCli < Formula
     if Hardware::CPU.arm?
 #         url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GithubPrivateRepositoryReleaseDownload
 #         sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
-        url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.5/kinetik--0.13.5.arm64_monterey.bottle.tar.gz"
+        url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.5/kinetik-cli--0.13.5.arm64_monterey.bottle.1.tar.gz"
         sha256 "04f71a4e4f736d7247a934740e91a199111c52522ce5c12854e688edfa154296"
     else
         url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GithubPrivateRepositoryReleaseDownload
@@ -40,9 +40,10 @@ class KinetikCli < Formula
   end
 
   on_linux do
-    url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-unknown-linux-gnu", :using => GithubPrivateRepositoryReleaseDownload
-#     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
-    sha256 "4f49bb353d920a01fd4fc3e2689c70f900848c35afb85bc72e2fb683bcb09ffd"
+    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.5/kinetik-cli--0.13.5.x86_64_linux.bottle.1.tar.gz"
+    sha256 "b8e8078427a79515d33fe5863ea0e8deb915b98d4b3e095f8e228444eb82ba0f"
+#     url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-unknown-linux-gnu", :using => GithubPrivateRepositoryReleaseDownload
+#     sha256 "4f49bb353d920a01fd4fc3e2689c70f900848c35afb85bc72e2fb683bcb09ffd"
     def install
         bin.install "kinetik-main-x86_64-unknown-linux-gnu" => "kinetik"
         chmod 0755, "#{bin}/kinetik"
