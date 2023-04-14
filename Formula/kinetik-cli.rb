ba2c946b15dbbdae18eaa3267da95b7413a4c308
Anne Wang <huiyang.wang621@gmail.com>
2023-04-14 15:45:47 +1200

    a

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -13,7 +13,7 @@ class KinetikCli < Formula
 #         url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-aarch64-apple-darwin", :using => GithubPrivateRepositoryReleaseDownload
 #         sha256 "8a7a2297d9700747c4d88e2eea9ad349c92ed89aa1c65de27ab088e8d1718650"
         url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.5/kinetik-cli--0.13.5.arm64_monterey.bottle.1.tar.gz"
-        sha256 "04f71a4e4f736d7247a934740e91a199111c52522ce5c12854e688edfa154296"
+        sha256 "afd5db0fec87f8ec7c13dd94db9e9100fffd10d56116e54500f4b87a12346a07"
     else
         url "https://github.com/section6nz/kinetik-base/releases/download/v0.13.5/kinetik-main-x86_64-apple-darwin", :using => GithubPrivateRepositoryReleaseDownload
         sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
