efe21ff155c4b9db2bd367a993057e5ded60f810
Anne Wang <huiyang.wang621@gmail.com>
2023-04-14 13:55:50 +1200

    a

--- Formula/backup.rb
+++ Formula/backup.rb
@@ -1,6 +1,6 @@
 require_relative "../lib/custom_download_strategy"
 
-class backup < Formula
+class Backup < Formula
   desc "This is the brew formula to install Kinetik CLI"
   homepage "https://github.com/section6nz/homebrew-kinetik-base"
   version "0.13.5"
